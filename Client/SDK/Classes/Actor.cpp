19e443b4a15b5f840ea9fe1fba5c73d64920b4c1
EchoHackCmd <EchoHackCmd@gmail.com>
2021-10-22 04:54:28 +0100

    Create Actor.cpp

---
+++ Client/SDK/Classes/Actor.cpp
@@ -0,0 +1,11 @@
+#include "Actor.h"
+
+#include "../Minecraft.h"
+
+auto Actor::onGround(void) -> bool* {
+    switch(Minecraft::sdkVer){
+        case MC_VER::v1_17_40_6:
+            return (bool*)((uintptr_t)(this) + 0x1D8);
+        break;
+    };
+};
