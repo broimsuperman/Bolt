d9adf65046e66171d26fa5ae3a4fd38168d97c66
EchoHackCmd <EchoHackCmd@gmail.com>
2021-10-22 07:28:10 +0100

    Update Utils.h

--- Client/Utils/Utils.h
+++ Client/Utils/Utils.h
@@ -20,4 +20,28 @@ public:
     static auto ptrToStr(uintptr_t) -> std::string;
 };
 
+template<typename T>
+class Vec2 {
+public:
+    T x = 0;
+    T y = 0;
+
+    Vec2(T x = 0, T y = 0){
+        this->x = x;
+        this->y = y;
+    };
+};
+
+template<typename T>
+class Vec3 : public Vec2<T> {
+public:
+    T z = 0;
+
+    Vec3(T x = 0, T y = 0, T z = 0){
+        this->x = x;
+        this->y = y;
+        this->z = z;
+    };
+};
+
 #endif /* CLIENT_UTILS_UTILS */
