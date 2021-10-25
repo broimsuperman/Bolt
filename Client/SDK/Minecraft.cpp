c3e8d72c0859bec6f41a4f8fab8aa75843ba6d48
EchoHackCmd <EchoHackCmd@gmail.com>
2021-10-25 08:45:17 +0100

    Update Minecraft.cpp

--- Client/SDK/Minecraft.cpp
+++ Client/SDK/Minecraft.cpp
@@ -11,3 +11,29 @@ auto Minecraft::getClientInstance(void) -> ClientInstance* {
     };
     return (ClientInstance*)nullptr;
 };
+
+auto Minecraft::getVersion(void) -> std::string {
+    auto base = (uintptr_t)(GetModuleHandleA("ucrtbase.dll"));
+    auto ver = base + 0xF12CB;
+
+    auto versionString = std::string(reinterpret_cast<char*>(ver));
+
+    if(versionString.find("\\") != std::string::npos){
+        int index = versionString.rfind("\\");
+        return versionString.substr(0, index);
+    };
+
+    return versionString;
+};
+
+auto Minecraft::setSdkToCurr(void) -> void {
+    auto version = getVersion();
+    
+    if(version.length() <= 0)
+        return;
+    
+    if(version.rfind("1.17.40.6") != std::string::npos) {
+        sdkVer = MC_VER::v1_17_40_6;
+        return Utils::debugLogF("Set SDK Version to 1.17.40.6\n");
+    };
+};
