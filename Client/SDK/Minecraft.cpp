9a1925429279bf87f3b2d00f7a4841cbb649eeeb
EchoHackCmd <EchoHackCmd@gmail.com>
2021-10-26 05:15:21 +0100

    Update Minecraft.cpp

--- Client/SDK/Minecraft.cpp
+++ Client/SDK/Minecraft.cpp
@@ -6,7 +6,7 @@ MC_VER Minecraft::sdkVer = MC_VER::Unknown;
 auto Minecraft::getClientInstance(void) -> ClientInstance* {
     switch(Minecraft::sdkVer){
         case MC_VER::v1_17_40_6:
-            return (ClientInstance*)Mem::findMultiLvlPtr((uintptr_t)(GetModuleHandleA("Minecraft.Windows.exe")) + 0x041FB270, { 0x0, 0x50, 0x0 });
+            return (ClientInstance*)Mem::findMultiLvlPtr((uintptr_t)(GetModuleHandleA("Minecraft.Windows.exe")) + 0x041FB278, { 0x0, 0x50, 0x0 });
         break;
         case MC_VER::v1_17_34_2:
             return (ClientInstance*)Mem::findMultiLvlPtr((uintptr_t)(GetModuleHandleA("Minecraft.Windows.exe")) + 0x041119F0, { 0x0, 0x50, 0x0 });
