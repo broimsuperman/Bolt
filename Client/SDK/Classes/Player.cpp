6a6263a06da922cb57fc476386b37fb370062e59
EchoHackCmd <EchoHackCmd@gmail.com>
2021-10-27 02:38:56 +0100

    Update Player.cpp

--- Client/SDK/Classes/Player.cpp
+++ Client/SDK/Classes/Player.cpp
@@ -1,1 +1,19 @@
 #include "Player.h"
+
+#include "../Minecraft.h"
+
+auto Player::knockback(Actor* entity, int paramA, float paramB, float paramC, float paramD, float paramE, float paramF) -> void {
+    using Knockback = void (__thiscall*)(Actor*, Actor*, int, float, float, float, float, float);
+    auto _Knockback = (Knockback)(this->VTable[0]);
+
+    switch(Minecraft::sdkVer){
+        case MC_VER::v1_17_40_6:
+            _Knockback = (Knockback)(this->VTable[276]);
+        break;
+        case MC_VER::v1_17_34_2:
+            _Knockback = (Knockback)(this->VTable[277]);
+        break;
+    };
+
+    _Knockback(this, entity, paramA, paramB, paramC, paramD, paramE, paramF);
+};
