279ee29c69074e152be723d034c60ee3f736be0c
EchoHackCmd <EchoHackCmd@gmail.com>
2021-10-27 04:01:48 +0100

    Update Player.h

--- Client/SDK/Classes/Player.h
+++ Client/SDK/Classes/Player.h
@@ -9,6 +9,7 @@
 class Player : public Actor {
 public:
     auto knockback(Actor*, int, float, float, float, float, float) -> void;
+    auto setSprinting(bool) -> void;
 };
 
 #endif /* CLIENT_SDK_CLASSES_PLAYER */
