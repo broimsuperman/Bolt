7d542460cc31f9556edf2fec661785cce41c389e
EchoHackCmd <EchoHackCmd@gmail.com>
2021-10-27 06:28:41 +0100

    Update Player.h

--- Client/SDK/Classes/Player.h
+++ Client/SDK/Classes/Player.h
@@ -12,6 +12,26 @@ public:
     auto setSprinting(bool) -> void;
     auto getSpeed(void) -> float;
     auto setSpeed(float) -> void;
+    auto getJumpPower(void) -> float;
+    auto hurtEffects(ActorDamageSource*, int, bool, bool) -> bool;
+    auto getMeleeWeaponDamageBonus(Mob*) -> __int64;
+    auto getMeleeKnockbackBonus(void) -> __int64;
+    auto travel(struct IMobMovementProxy*, float, float, float) -> void;
+    auto travel(float, float, float) -> void;
+    auto applyFinalFriction(float, bool) -> void;
+    auto updateWalkAnim(void) -> void;
+    auto aiStep(IMobMovementProxy*) -> void;
+    auto aiStep(void) -> void;
+    auto pushActors(void) -> void;
+    auto lookAt(Actor*, float, float) -> void;
+    auto isLookingAtAnEntity(void) -> bool;
+    auto checkSpawnRules(bool) -> bool;
+    auto checkSpawnObstruction(void) -> bool;
+    auto getAttackAnim(float) -> float;
+    auto getItemUseDuration(void) -> __int64;
+    auto getItemUseStartupProgress(void) -> float;
+    auto getItemUseIntervalProgress(void) -> float;
+    auto getItemUseIntervalAxis(void) -> __int64;
 };
 
 #endif /* CLIENT_SDK_CLASSES_PLAYER */
