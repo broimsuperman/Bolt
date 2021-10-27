090d429c9d7d46bc97dcd69a6aed2ffafac4e1f2
EchoHackCmd <EchoHackCmd@gmail.com>
2021-10-27 01:24:05 +0100

    1.17.34 and 1.17.40 support

--- Client/SDK/Classes/MinecraftGame.cpp
+++ Client/SDK/Classes/MinecraftGame.cpp
@@ -6,10 +6,22 @@ auto MinecraftGame::getFont(void) -> Font* {
         case MC_VER::v1_17_40_6:
             return *(Font**)((uintptr_t)(this) + 0x110);
         break;
+        case MC_VER::v1_17_34_2:
+            return *(Font**)((uintptr_t)(this) + 0x110);
+        break;
     };
     return (Font*)nullptr;
 };
 
 auto MinecraftGame::canUseKeys(void) -> bool {
-    return *(bool*)((uintptr_t)(this) + 0x2F8);
+    switch(Minecraft::sdkVer){
+        case MC_VER::v1_17_40_6:
+            return *(bool*)((uintptr_t)(this) + 0x2F8);
+        break;
+        case MC_VER::v1_17_34_2:
+            return *(bool*)((uintptr_t)(this) + 0x2F8);
+        break;
+    };
+    
+    return false;
 };
