dc86b6d7e5c71d333fb9954b45b52682c5188a20
EchoHackCmd <EchoHackCmd@gmail.com>
2021-10-31 10:28:45 +0000

    Create BlockSource.cpp

---
+++ Client/SDK/Classes/BlockSource.cpp
@@ -0,0 +1,20 @@
+#include "BlockSource.h"
+
+#include "../Minecraft.h"
+
+auto BlockSource::getBlock(Vec3<int>* blockPos) -> Block* {
+    using GetBlock = Block* (__thiscall*)(BlockSource*, Vec3<int>*);
+    static auto _GetBlock = (GetBlock)nullptr;
+
+    if(_GetBlock == nullptr)
+        switch(Minecraft::sdkVer){
+            case MC_VER::v1_17_40_6:
+                _GetBlock = (GetBlock)(Mem::findSig("48 89 5C 24 ? 57 48 83 EC 20 48 8B F9 48 8B DA 8B 4A 04 0F BF 47 3A"));
+            break;
+            case MC_VER::v1_17_34_2:
+                _GetBlock = (GetBlock)(Mem::findSig("48 89 5C 24 ? 57 48 83 EC 20 48 8B F9 48 8B DA 8B 4A 04 0F BF 47 3A"));
+            break;
+        };
+
+    return _GetBlock(this, blockPos);
+};
