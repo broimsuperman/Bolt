f30cc545a95c1e5539d6e2fe57e0b8a842ae9c1d
EchoHackCmd <EchoHackCmd@gmail.com>
2021-10-31 05:26:35 +0000

    Create Level.h

---
+++ Client/SDK/Classes/Level.h
@@ -0,0 +1,12 @@
+#ifndef CLIENT_SDK_CLASSES_LEVEL
+#define CLIENT_SDK_CLASSES_LEVEL
+
+#include "../../Mem/Mem.h"
+#include "../../Utils/Utils.h"
+
+class Level {
+public:
+    //
+};
+
+#endif /* CLIENT_SDK_CLASSES_LEVEL */
