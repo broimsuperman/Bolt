685adbf363c4b7435ac381e504b89d4fb0a12c5b
EchoHackCmd <EchoHackCmd@gmail.com>
2021-10-31 05:26:37 +0000

    Create Dimension.h

---
+++ Client/SDK/Classes/Dimension.h
@@ -0,0 +1,12 @@
+#ifndef CLIENT_SDK_CLASSES_DIMENSION
+#define CLIENT_SDK_CLASSES_DIMENSION
+
+#include "../../Mem/Mem.h"
+#include "../../Utils/Utils.h"
+
+class Dimension {
+public:
+    //
+};
+
+#endif /* CLIENT_SDK_CLASSES_DIMENSION */
