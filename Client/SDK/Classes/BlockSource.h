765f385278ebaf7badd382ed385810702c03b13c
EchoHackCmd <EchoHackCmd@gmail.com>
2021-10-31 05:26:39 +0000

    Create BlockSource.h

---
+++ Client/SDK/Classes/BlockSource.h
@@ -0,0 +1,12 @@
+#ifndef CLIENT_SDK_CLASSES_BLOCKSOURCE
+#define CLIENT_SDK_CLASSES_BLOCKSOURCE
+
+#include "../../Mem/Mem.h"
+#include "../../Utils/Utils.h"
+
+class BlockSource {
+public:
+    //
+};
+
+#endif /* CLIENT_SDK_CLASSES_BLOCKSOURCE */
