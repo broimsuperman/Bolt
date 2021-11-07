289f42188c0518f26198b747521a9c57b4aa9b7e
EchoHackCmd <EchoHackCmd@gmail.com>
2021-11-07 05:02:29 +0000

    Update ClientInstance.cpp

--- Client/SDK/Classes/ClientInstance.cpp
+++ Client/SDK/Classes/ClientInstance.cpp
@@ -3,6 +3,9 @@
 
 auto ClientInstance::getLocalPlayer(void) -> Player* {
     switch(Minecraft::sdkVer){
+        case MC_VER::v1_17_41_1:
+            return *(Player**)((uintptr_t)(this) + 0x138);
+        break;
         case MC_VER::v1_17_40_6:
             return *(Player**)((uintptr_t)(this) + 0x138);
         break;
@@ -15,6 +18,9 @@ auto ClientInstance::getLocalPlayer(void) -> Player* {
 
 auto ClientInstance::getMinecraftGame(void) -> MinecraftGame* {
     switch(Minecraft::sdkVer){
+        case MC_VER::v1_17_41_1:
+            return *(MinecraftGame**)((uintptr_t)(this) + 0xA8);
+        break;
         case MC_VER::v1_17_40_6:
             return *(MinecraftGame**)((uintptr_t)(this) + 0xA8);
         break;
