db2ba24f82332fd6dfa1d16709920aed06b4c0af
EchoHackCmd <EchoHackCmd@gmail.com>
2021-11-07 05:02:27 +0000

    Update MinecraftGame.cpp

--- Client/SDK/Classes/MinecraftGame.cpp
+++ Client/SDK/Classes/MinecraftGame.cpp
@@ -3,6 +3,9 @@
 
 auto MinecraftGame::getFont(void) -> Font* {
     switch(Minecraft::sdkVer){
+        case MC_VER::v1_17_41_1:
+            return *(Font**)((uintptr_t)(this) + 0x108);
+        break;
         case MC_VER::v1_17_40_6:
             return *(Font**)((uintptr_t)(this) + 0x108);
         break;
@@ -15,6 +18,9 @@ auto MinecraftGame::getFont(void) -> Font* {
 
 auto MinecraftGame::canUseKeys(void) -> bool {
     switch(Minecraft::sdkVer){
+        case MC_VER::v1_17_41_1:
+            return *(bool*)((uintptr_t)(this) + 0x2F8);
+        break;
         case MC_VER::v1_17_40_6:
             return *(bool*)((uintptr_t)(this) + 0x2F8);
         break;
