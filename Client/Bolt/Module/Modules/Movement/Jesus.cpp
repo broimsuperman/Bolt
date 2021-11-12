60b1eba4c49526feb8c07bdec99002de648ae02f
EchoHackCmd <EchoHackCmd@gmail.com>
2021-11-12 06:38:47 +0000

    Update Jesus.cpp

--- Client/Bolt/Module/Modules/Movement/Jesus.cpp
+++ Client/Bolt/Module/Modules/Movement/Jesus.cpp
@@ -3,8 +3,10 @@
 auto Jesus::onGameMode(GameMode* GM) -> void {
     if(GM == nullptr || GM->player == nullptr)
         return;
+    
     auto player = GM->player;
+    auto oMotion = player->getMotion();
 
     if(player->isInWater() || player->hasEnteredWater() || player->isInLava())
-        player->jumpFromGround();
+        player->setMotion(Vec3<float>(oMotion.x, .38f, oMotion.z));
 };
