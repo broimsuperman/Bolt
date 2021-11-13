7facce32288ce43d544351aab64a5f73bf2de030
EchoHackCmd <EchoHackCmd@gmail.com>
2021-11-13 03:21:19 +0000

    Update Packet.h

--- Client/SDK/Classes/Packet.h
+++ Client/SDK/Classes/Packet.h
@@ -1,6 +1,9 @@
 #ifndef CLIENT_SDK_CLASSES_PACKET
 #define CLIENT_SDK_CLASSES_PACKET
 
+#include "../../Mem/Mem.h"
+#include "../../Utils/Utils.h"
+
 class Packet {
 public:
     uintptr_t** VTable;
