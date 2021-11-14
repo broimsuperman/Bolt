150a6cfc90acdfa8800454208d2fef10a76505d5
EchoHackCmd <EchoHackCmd@gmail.com>
2021-11-14 18:02:26 +0000

    Update BoostHit.cpp

--- Client/Bolt/Module/Modules/Combat/BoostHit.cpp
+++ Client/Bolt/Module/Modules/Combat/BoostHit.cpp
@@ -22,8 +22,14 @@ auto BoostHit::onPlayerAttack(GameMode* GM, Actor* victim) -> void {
 
     auto dist = sqrt(dX * dX + dY * dY + dZ * dZ);
     auto angles = Vec2<float>(-(float)atan2(dY, dist) * (180.f / PI), (float)atan2(dZ, dX) * (180.f / PI) + 90.f);
+    
+    auto tooClose = (dist <= 2.f);
+    auto yaw = angles.y + (tooClose ? -90.f : 90.f);
 
-    auto yaw = angles.y + 65.f;
+    auto speed = this->boost;
 
-    player->setMotion(Vec3<float>(cos((yaw) * (PI / 180.0f)) * boost, player->getMotion().y ,sin((yaw) * (PI / 180.0f)) * boost));
+    if(tooClose)
+        speed += 2.f;
+
+    player->setMotion(Vec3<float>(cos((yaw) * (PI / 180.0f)) * speed, player->getMotion().y ,sin((yaw) * (PI / 180.0f)) * speed));
 };
