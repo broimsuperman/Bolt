3642ce897f800dfcb6e44ac74e7f9e721f9a90a8
EchoHackCmd <EchoHackCmd@gmail.com>
2021-11-14 18:02:28 +0000

    Update BoostHit.h

--- Client/Bolt/Module/Modules/Combat/BoostHit.h
+++ Client/Bolt/Module/Modules/Combat/BoostHit.h
@@ -10,7 +10,7 @@ public:
     };
     auto onPlayerAttack(GameMode*, Actor*) -> void override;
 private:
-    float boost = 1.f;
+    float boost = 1.2f;
 };
 
 #endif /* CLIENT_BOLT_MODULE_MODULES_COMBAT_BOOSTHIT */
