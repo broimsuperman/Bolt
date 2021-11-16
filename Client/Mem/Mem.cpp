0401ad2255de8742984625b448819c565394969d
EchoHackCmd <EchoHackCmd@gmail.com>
2021-11-16 20:41:30 +0000

    Update Mem.cpp

--- Client/Mem/Mem.cpp
+++ Client/Mem/Mem.cpp
@@ -1,64 +1,14 @@
 #include "Mem.h"
 
-auto Mem::findSig(const char* sig) -> uintptr_t {
+auto Mem::findSig(const char* sig, const char* mod) -> uintptr_t {
     const char* pattern = sig;
 	uintptr_t firstMatch = 0;
-	static const uintptr_t rangeStart = (uintptr_t)GetModuleHandleA("Minecraft.Windows.exe");
-	static MODULEINFO miModInfo;
-	static bool init = false;
-	if (!init) {
-		init = true;
-		GetModuleInformation(GetCurrentProcess(), (HMODULE)rangeStart, &miModInfo, sizeof(MODULEINFO));
-	}
-	static const uintptr_t rangeEnd = rangeStart + miModInfo.SizeOfImage;
-
-    BYTE patByte = GET_BYTE(pattern);
-	const char* oldPat = pattern;
-
-    for (uintptr_t pCur = rangeStart; pCur < rangeEnd; pCur++){
-        if (!*pattern)
-            return firstMatch;
-        
-        while (*(PBYTE)pattern == ' ')
-            pattern++;
-        
-        if (!*pattern)
-            return firstMatch;
-        
-        if (oldPat != pattern){
-            oldPat = pattern;
-            if (*(PBYTE)pattern != '\?')
-                patByte = GET_BYTE(pattern);
-        };
-
-        if (*(PBYTE)pattern == '\?' || *(BYTE*)pCur == patByte){
-            if (!firstMatch)
-                firstMatch = pCur;
-            
-            if (!pattern[2])
-                return firstMatch;
-            
-            pattern += 2;
-        }
-        else {
-            pattern = sig;
-            firstMatch = 0;
-        };
-    };
-    return NULL;
-};
-
-auto Mem::findSigInMod(const char* sig, const char* mod) -> uintptr_t {
-    const char* pattern = sig;
-	uintptr_t firstMatch = 0;
-	static const uintptr_t rangeStart = (uintptr_t)GetModuleHandleA(mod);
-	static MODULEINFO miModInfo;
-	static bool init = false;
-	if (!init) {
-		init = true;
-		GetModuleInformation(GetCurrentProcess(), (HMODULE)rangeStart, &miModInfo, sizeof(MODULEINFO));
-	}
-	static const uintptr_t rangeEnd = rangeStart + miModInfo.SizeOfImage;
+	const uintptr_t rangeStart = (uintptr_t)GetModuleHandleA(mod);
+	MODULEINFO miModInfo;
+	
+    GetModuleInformation(GetCurrentProcess(), (HMODULE)rangeStart, &miModInfo, sizeof(MODULEINFO));
+	
+    const uintptr_t rangeEnd = rangeStart + miModInfo.SizeOfImage;
 
     BYTE patByte = GET_BYTE(pattern);
 	const char* oldPat = pattern;
