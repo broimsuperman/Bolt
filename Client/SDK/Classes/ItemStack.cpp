e1fdafdcc7401ce48ad9b7257856c7bd03b0d6bf
EchoHackCmd <EchoHackCmd@gmail.com>
2021-11-25 20:52:37 +0000

    Update ItemStack.cpp

--- Client/SDK/Classes/ItemStack.cpp
+++ Client/SDK/Classes/ItemStack.cpp
@@ -2,42 +2,44 @@
 
 #include "../Minecraft.h"
 
-auto Item::setStackedByData(bool state) -> void {
-    using SetStacked = void (__thiscall*)(Item*, bool);
-    static auto _SetStacked = (SetStacked)nullptr;
-
-    if(_SetStacked == nullptr)
-        switch(Minecraft::sdkVer) {
-            case MC_VER::v1_17_41_1:
-                _SetStacked = (SetStacked)(Mem::findSig("80 A1 ? ? ? ? ? 48 8B C1 C0 E2 02 08"));
-            break;
-            case MC_VER::v1_17_40_6:
-                _SetStacked = (SetStacked)(Mem::findSig("80 A1 ? ? ? ? ? 48 8B C1 C0 E2 02 08"));
-            break;
-            case MC_VER::v1_17_34_2:
-                _SetStacked = (SetStacked)(Mem::findSig("80 A1 ? ? ? ? ? 48 8B C1 C0 E2 02 08"));
-            break;
-        };
-
-    _SetStacked(this, state);
-};
-
 auto Item::setMaxStackSize(UCHAR stackSize) -> void {
     using SetMaxStackSize = void (__thiscall*)(Item*, UCHAR);
     static auto _SetMaxStackSize = (SetMaxStackSize)nullptr;
 
-    if(_SetMaxStackSize == nullptr)
+    if(_SetMaxStackSize == nullptr) {
         switch(Minecraft::sdkVer) {
             case MC_VER::v1_17_41_1:
-                _SetMaxStackSize = (SetMaxStackSize)(Mem::findSig("88 51 78 48 8B C1 C3"));
+                _SetMaxStackSize = (SetMaxStackSize)(this->VTable[24]);
             break;
             case MC_VER::v1_17_40_6:
-                _SetMaxStackSize = (SetMaxStackSize)(Mem::findSig("88 51 78 48 8B C1 C3"));
+                _SetMaxStackSize = (SetMaxStackSize)(this->VTable[24]);
             break;
             case MC_VER::v1_17_34_2:
-                _SetMaxStackSize = (SetMaxStackSize)(Mem::findSig("88 51 78 48 8B C1 C3"));
+                _SetMaxStackSize = (SetMaxStackSize)(this->VTable[24]);
             break;
         };
+    };
 
     _SetMaxStackSize(this, stackSize);
+};
+
+auto Item::setStackedByData(bool state) -> void {
+    using SetStacked = void (__thiscall*)(Item*, bool);
+    static auto _SetStacked = (SetStacked)nullptr;
+
+    if(_SetStacked == nullptr) {
+        switch(Minecraft::sdkVer) {
+            case MC_VER::v1_17_41_1:
+                _SetStacked = (SetStacked)(this->VTable[25]);
+            break;
+            case MC_VER::v1_17_40_6:
+                _SetStacked = (SetStacked)(this->VTable[25]);
+            break;
+            case MC_VER::v1_17_34_2:
+                _SetStacked = (SetStacked)(this->VTable[25]);
+            break;
+        };
+    };
+
+    _SetStacked(this, state);
 };
