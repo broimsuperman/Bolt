f72e1991c7898eef5c9b7f7bc32e1ea68f39d9ab
EchoHackCmd <EchoHackCmd@gmail.com>
2021-11-25 22:36:57 +0000

    Update TestModule.cpp

--- Client/Bolt/Module/Modules/Other/TestModule.cpp
+++ Client/Bolt/Module/Modules/Other/TestModule.cpp
@@ -5,46 +5,11 @@
 #include "../../../Client/Client.h"
 
 auto TestModule::onEnable(void) -> void {
-    auto instance = Minecraft::getClientInstance();
-
-    if(instance == nullptr)
-        return Utils::debugLogF("Client Instance is invalid!");
-    
-    auto player = instance->getLocalPlayer();
-
-    if(player == nullptr)
-        return Utils::debugLogF("Local Player is invalid!");
-
-    auto supplies = player->getSupplies();
-
-    if(supplies == nullptr)
-        return this->setState(false);
-    
-    auto inventory = supplies->getInventory();
-
-    if(inventory == nullptr)
-        return this->setState(false);
-    
-    auto itemA = inventory->getItem(0);
-
-    if(itemA == nullptr || itemA->isNull())
-        this->displayToChat("Item invalid!");
-    else
-        this->displayToChat(std::to_string(itemA->isBlock()));
-    
-    this->setState(false);
+    return;
 };
 
 auto TestModule::onTick(void) -> void {
-    auto instance = Minecraft::getClientInstance();
-
-    if(instance == nullptr)
-        return;
-    
-    auto player = instance->getLocalPlayer();
-
-    if(player == nullptr)
-        return;
+    return;
 };
 
 auto TestModule::onRender(RenderUtils* r) -> void {
