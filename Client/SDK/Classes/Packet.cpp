8e65fb4f172e1ab5736a6f9862c9b2a61a1a3fe6
EchoHackCmd <EchoHackCmd@gmail.com>
2021-11-26 03:39:05 +0000

    Update Packet.cpp

--- Client/SDK/Classes/Packet.cpp
+++ Client/SDK/Classes/Packet.cpp
@@ -14,3 +14,21 @@ public:
 auto Packet::getName(void) -> std::string {
     return ((_Packet*)(this))->getName();
 };
+
+/* Text Packet */
+
+TextPacket::TextPacket() {
+    memset(this, 0, sizeof(this));
+    
+    switch(Minecraft::sdkVer) {
+        case MC_VER::v1_17_41_1:
+            this->VTable = (uint64_t**)((uintptr_t)(GetModuleHandleA("Minecraft.Windows.exe")) + 0x03E7D5D0);
+        break;
+        case MC_VER::v1_17_40_6:
+            this->VTable = (uint64_t**)((uintptr_t)(GetModuleHandleA("Minecraft.Windows.exe")) + 0x03E7C5D0);
+        break;
+        case MC_VER::v1_17_34_2:
+            this->VTable = (uint64_t**)((uintptr_t)(GetModuleHandleA("Minecraft.Windows.exe")) + 0x03D9D7C8);
+        break;
+    };
+};
