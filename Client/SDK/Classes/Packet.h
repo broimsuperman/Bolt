52e5a8481d5c636aa163cb91b965b1197c162d6e
EchoHackCmd <EchoHackCmd@gmail.com>
2021-11-26 03:39:08 +0000

    Update Packet.h

--- Client/SDK/Classes/Packet.h
+++ Client/SDK/Classes/Packet.h
@@ -11,4 +11,28 @@ public:
     auto getName(void) -> std::string;
 };
 
+class TextPacket : public Packet {
+public:
+    TextPacket();
+private:
+    char pad_0008[40];
+public:
+    public:
+    unsigned __int8 type;
+private:
+    unsigned char padB[7];
+public:
+    std::string authorName;
+    std::string messageContents;
+private:
+    unsigned char padC[24];
+public:
+    bool translationNeeded = false;
+private:
+    unsigned char padD[7];
+public:
+    std::string xboxUserId;
+    std::string platformChatId;
+};
+
 #endif /* CLIENT_SDK_CLASSES_PACKET */
