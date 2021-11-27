f962c5ddad288de580398ca17a361d4cf53b65e5
EchoHackCmd <EchoHackCmd@gmail.com>
2021-11-27 19:24:20 +0000

    Update Surround.h

--- Client/Bolt/Module/Modules/Combat/Surround.h
+++ Client/Bolt/Module/Modules/Combat/Surround.h
@@ -10,6 +10,8 @@ public:
     };
     auto onGameMode(GameMode*) -> void override;
     auto tryScaffold(GameMode*, Vec3<float>) -> bool;
+public:
+    std::chrono::time_point<std::chrono::steady_clock> savedTime = std::chrono::high_resolution_clock::now();
 };
 
 #endif /* CLIENT_BOLT_MODULE_MODULES_COMBAT_SURROUND */
