e677fbe473808c6f1d989d3585887e89b9e161ac
EchoHackCmd <EchoHackCmd@gmail.com>
2021-12-01 04:18:24 +0000

    Update LevelRenderer.cpp

--- Client/SDK/Classes/LevelRenderer.cpp
+++ Client/SDK/Classes/LevelRenderer.cpp
@@ -6,6 +6,9 @@ auto LevelRenderer::getOrigin(void) -> Vec3<float> {
     auto origin = (Vec3<float>*)nullptr;
 
     switch(Minecraft::sdkVer) {
+        case MC_VER::v1_18_0_02:
+                origin = (Vec3<float>*)((uintptr_t)(this) + 0x87C);
+            break;
             case MC_VER::v1_17_41_1:
                 origin = (Vec3<float>*)((uintptr_t)(this) + 0x854);
             break;
