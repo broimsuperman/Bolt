eff1f305fea2cfcaf108958058126579cf15b02a
EchoHackCmd <EchoHackCmd@gmail.com>
2021-12-03 03:52:56 +0000

    TextPacket for 1.18

--- Client/SDK/Classes/Packet.cpp
+++ Client/SDK/Classes/Packet.cpp
@@ -18,9 +18,10 @@ auto Packet::getName(void) -> std::string {
 /* Text Packet */
 
 TextPacket::TextPacket() {
-    memset(this, 0, sizeof(this));
-    
     switch(Minecraft::sdkVer) {
+        case MC_VER::v1_18_0_02:
+            this->VTable = (uint64_t**)((uintptr_t)(GetModuleHandleA("Minecraft.Windows.exe")) + 0x03E9A700);
+        break;
         case MC_VER::v1_17_41_1:
             this->VTable = (uint64_t**)((uintptr_t)(GetModuleHandleA("Minecraft.Windows.exe")) + 0x03E7D5D0);
         break;
@@ -31,4 +32,11 @@ TextPacket::TextPacket() {
             this->VTable = (uint64_t**)((uintptr_t)(GetModuleHandleA("Minecraft.Windows.exe")) + 0x03D9D7C8);
         break;
     };
+    
+    /*auto sig = Mem::findSig("48 8D 05 ? ? ? ? 48 89 01 C6 41 30 ?");
+
+    if(!sig)
+        return;
+    
+    this->VTable = reinterpret_cast<uintptr_t**>(sig + *(int*)(sig + 3) + 7);*/
 };
