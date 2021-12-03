9a15a50d5850559d8a868ecd3b70405a46309311
EchoHackCmd <EchoHackCmd@gmail.com>
2021-12-03 03:52:45 +0000

    RakNet stuff for 1.18

--- Client/SDK/Classes/LoopbackPacketSender.cpp
+++ Client/SDK/Classes/LoopbackPacketSender.cpp
@@ -6,6 +6,9 @@ auto RakNetInstance::getIP(void) -> std::string {
     std::string serverIP = std::string("Unknown");
 
     switch(Minecraft::sdkVer){
+        case MC_VER::v1_18_0_02:
+            serverIP = std::string((char*)((uintptr_t)(this) + 0x3D0));
+        break;
         case MC_VER::v1_17_41_1:
             serverIP = std::string((char*)((uintptr_t)(this) + 0x3D0));
         break;
@@ -24,6 +27,9 @@ auto RakNetInstance::getDynamicIP(void) -> std::string {
     std::string dynamicIP = std::string("Unknown");
 
     switch(Minecraft::sdkVer){
+        case MC_VER::v1_18_0_02:
+            dynamicIP = std::string(*(char**)((uintptr_t)(this) + 0x3F0));
+        break;
         case MC_VER::v1_17_41_1:
             dynamicIP = std::string(*(char**)((uintptr_t)(this) + 0x3F0));
         break;
@@ -40,21 +46,27 @@ auto RakNetInstance::getDynamicIP(void) -> std::string {
 
 auto NetworkHandler::getRakNet(void) -> RakNetInstance* {
     switch(Minecraft::sdkVer){
+        case MC_VER::v1_18_0_02:
+            return *(RakNetInstance**)((uintptr_t)(this) + 0x58);
+        break;
         case MC_VER::v1_17_41_1:
-            return *( RakNetInstance**)((uintptr_t)(this) + 0x58);
+            return *(RakNetInstance**)((uintptr_t)(this) + 0x58);
         break;
         case MC_VER::v1_17_40_6:
-            return *( RakNetInstance**)((uintptr_t)(this) + 0x58);
+            return *(RakNetInstance**)((uintptr_t)(this) + 0x58);
         break;
         case MC_VER::v1_17_34_2:
-            return *( RakNetInstance**)((uintptr_t)(this) + 0x58);
+            return *(RakNetInstance**)((uintptr_t)(this) + 0x58);
         break;
     };
-    return *( RakNetInstance**)nullptr;
+    return *(RakNetInstance**)nullptr;
 };
 
 auto LoopbackPacketSender::getNetworkHandler(void) -> NetworkHandler* {
     switch(Minecraft::sdkVer){
+        case MC_VER::v1_18_0_02:
+            return *(NetworkHandler**)((uintptr_t)(this) + 0x10);
+        break;
         case MC_VER::v1_17_41_1:
             return *(NetworkHandler**)((uintptr_t)(this) + 0x10);
         break;
