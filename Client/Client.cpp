d813823b81fef547d45f4e5bd1a111cdcd245cb8
EchoHackCmd <EchoHackCmd@gmail.com>
2021-12-04 19:13:58 +0000

    Update Client.cpp

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -2,9 +2,6 @@
 
 Client::Client(std::string name, std::string version){
     Utils::setDebugPath(std::string(Utils::getDebugPath() + "\\" + name).c_str());
-    
-    Utils::rmDebugPath();
-    
     Utils::debugLogF(std::string("Initializing Client with name: " + name).c_str());
 
     this->name = name;
