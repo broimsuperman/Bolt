cb1f707dbe7f5b24465ca8f47613c138b4eaa5d4
EchoHackCmd <EchoHackCmd@gmail.com>
2021-12-05 13:35:25 +0000

    Update Uninject.cpp

--- Client/Bolt/Module/Modules/Other/Uninject.cpp
+++ Client/Bolt/Module/Modules/Other/Uninject.cpp
@@ -1,5 +1,18 @@
 #include "Uninject.h"
 
+#include "../../../Category/Category.h"
+#include "../../../Manager/Manager.h"
+#include "../../../Client/Client.h"
+
 auto Uninject::onEnable(void) -> void {
+    
+    for(auto c : this->getManager()->getCategories()) {
+        for(auto m : c->getModules()) {
+            m->isEnabled = false;
+        };
+    };
+
+    this->getManager()->emptyEntityMap();
+
     Minecraft::removeThread();
 };
