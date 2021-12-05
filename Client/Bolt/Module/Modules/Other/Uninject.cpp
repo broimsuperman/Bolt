b48152c338052baa0b2f5307f444f8b0206efc11
EchoHackCmd <EchoHackCmd@gmail.com>
2021-12-05 06:20:09 +0000

    Update Uninject.cpp

--- Client/Bolt/Module/Modules/Other/Uninject.cpp
+++ Client/Bolt/Module/Modules/Other/Uninject.cpp
@@ -1,9 +1,5 @@
 #include "Uninject.h"
 
-#include "../../../Category/Category.h"
-#include "../../../Manager/Manager.h"
-#include "../../../Client/Client.h"
-
 auto Uninject::onEnable(void) -> void {
     Minecraft::removeThread();
 };
