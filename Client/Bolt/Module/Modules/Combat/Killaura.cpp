f86350b349fd658441946a9040a5abb7c41fa330
EchoHackCmd <EchoHackCmd@gmail.com>
2021-12-21 21:32:25 +0000

    More efficient Killaura

--- Client/Bolt/Module/Modules/Combat/Killaura.cpp
+++ Client/Bolt/Module/Modules/Combat/Killaura.cpp
@@ -16,34 +16,37 @@ auto Killaura::onGameMode(GameMode* GM) -> void {
     
     auto player = GM->player;
     auto myPos = *player->getPos();
+    auto attackCount = (int)0;
 
-    int count = 0;
-    
     for(auto [runtimeId, entity] : this->getManager()->getEntityMap()) {
-        if(runtimeId == player->getRuntimeID())
+        if(!canAttackEnt(player, entity))
             continue;
         
-        if(entity == nullptr || entity->VTable == nullptr)
-            continue;
+        if(attackCount >= 4)
+            break;
         
-        if(!entity->isAlive())
-            continue;
+        auto distToTarget = entity->getPos()->distanceTo(myPos); /* Calculate the distance between entities (target - player) */
         
-        auto pos = *entity->getPos();
-        
-        auto dX = myPos.x - pos.x;
-        auto dY = myPos.y - pos.y;
-        auto dZ = myPos.z - pos.z;
-
-        auto dist = sqrt(dX * dX + dY * dY + dZ * dZ);
-
-        if(dist <= 8.f) {
+        if(distToTarget <= this->range) {
             GM->attack(entity);
             player->swing();
-            count++;
+            attackCount++;
         };
-
-        if(count >= 12)
-            break;
     };
+};
+
+auto Killaura::canAttackEnt(Player* player, Actor* entity) -> bool {
+    if(entity == nullptr || entity->VTable == nullptr)
+        return false;
+    
+    if(!entity->isAlive() || entity->getRuntimeID() <= 0 || player->getRuntimeID() == entity->getRuntimeID())
+        return false;
+    
+    if(entity->isInCreativeMode())
+        return false;
+    
+    if(!player->canAttack(entity, false))
+        return false;
+    
+    return true;
 };
