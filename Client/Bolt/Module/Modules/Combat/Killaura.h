1082a06e941baded03b6eea81a238b777c64dbb3
EchoHackCmd <EchoHackCmd@gmail.com>
2021-12-21 21:53:21 +0000

    Update Killaura.h

--- Client/Bolt/Module/Modules/Combat/Killaura.h
+++ Client/Bolt/Module/Modules/Combat/Killaura.h
@@ -11,6 +11,10 @@ public:
 public:
     auto onTick(void) -> void override;
     auto onGameMode(GameMode*) -> void override;
+public:
+    auto canAttackEnt(Player*, Actor*) -> bool;
+public:
+    float range = 10.f;
 };
 
 #endif /* CLIENT_BOLT_MODULE_MODULES_COMBAT_KILLAURA */
