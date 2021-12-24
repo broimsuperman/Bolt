722cbb9ecf3843ec25524235939e40af934e171b
EchoHackCmd <EchoHackCmd@gmail.com>
2021-12-24 23:29:14 +0000

    Update Level.h

--- Client/SDK/Classes/Level.h
+++ Client/SDK/Classes/Level.h
@@ -4,9 +4,14 @@
 #include "../../Mem/Mem.h"
 #include "../../Utils/Utils.h"
 
+class Actor;
+class Player;
+
 class Level {
 public:
-    //
+    uintptr_t** VTable;
+public:
+    auto fetchEntity(__int64, bool) -> Actor*;
 };
 
 #endif /* CLIENT_SDK_CLASSES_LEVEL */
