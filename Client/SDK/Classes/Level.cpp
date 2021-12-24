3a654b20275fbadae37a16fd981dc4a1e4888875
EchoHackCmd <EchoHackCmd@gmail.com>
2021-12-24 23:29:17 +0000

    Create Level.cpp

---
+++ Client/SDK/Classes/Level.cpp
@@ -0,0 +1,28 @@
+#include "Level.h"
+
+#include "../Minecraft.h"
+
+auto Level::fetchEntity(__int64 runtimeId, bool paramB) -> Actor* {
+    using FetchEntity = Actor* (__thiscall*)(Level*, __int64, bool);
+    auto _FetchEntity = (FetchEntity)nullptr;
+
+    switch(Minecraft::sdkVer) {
+        case MC_VER::v1_18_1_20:
+            _FetchEntity = (FetchEntity)(this->VTable[49]);
+        break;
+        case MC_VER::v1_18_0_02:
+            _FetchEntity = (FetchEntity)(this->VTable[49]);
+        break;
+        case MC_VER::v1_17_41_1:
+            _FetchEntity = (FetchEntity)(this->VTable[49]);
+        break;
+        case MC_VER::v1_17_40_6:
+            _FetchEntity = (FetchEntity)(this->VTable[49]);
+        break;
+        case MC_VER::v1_17_34_2:
+            _FetchEntity = (FetchEntity)(this->VTable[49]);
+        break;
+    };
+
+    return _FetchEntity(this, runtimeId, paramB);
+};
