73a5003298ff461b4e7e24037aa09c81b52ae64c
EchoHackCmd <EchoHackCmd@gmail.com>
2021-12-24 20:45:20 +0000

    Update EntityJetpack.cpp

--- Client/Bolt/Module/Modules/Movement/EntityJetpack.cpp
+++ Client/Bolt/Module/Modules/Movement/EntityJetpack.cpp
@@ -15,18 +15,14 @@ auto EntityJetpack::onGameMode(GameMode* GM) -> void {
     
     auto player = GM->player;
     auto bodyRot = *GM->player->getBodyRot();
-
-    auto isPlayerRiding = [](Player* player, Actor* entity) -> bool {
-        return (entity->isPassenger(player) || entity->getFirstPassenger() != nullptr && entity->getFirstPassenger()->getRuntimeID() == player->getRuntimeID());
-    };
     
     for(auto [runtimeId, entity] : this->getManager()->getEntityMap()) {
-        if(isPlayerRiding(player, entity) && this->getManager()->isHoldingKey(jetpackMod->key)) {
+        if(player->isRidingMob(entity) && this->getManager()->isHoldingKey(jetpackMod->key)) {
             auto angles = Vec2<float>((bodyRot.x) * -(PI / 180.f), (bodyRot.y + 90.f) * PI / 180.f);
             auto motion = Vec3<float>(cos(angles.y) * cos(angles.x) * speed, sin(angles.x) * speed, sin(angles.y) * cos(angles.x) * speed);
             
-            player->setMotion(motion);
             entity->setMotion(motion);
+            player->setMotion(motion);
             
             auto currRot = *entity->getBodyRot();
             *entity->getBodyRot() = Vec2<float>(bodyRot.x >= ((Player*)(entity))->getMaxHeadXRot() ? currRot.x : bodyRot.x, bodyRot.y);
