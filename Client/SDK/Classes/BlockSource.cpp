c1aef4ac2eed392ac5b749520529ac435a02b805
EchoHackCmd <EchoHackCmd@gmail.com>
2021-12-25 06:40:24 +0000

    Update BlockSource.cpp

--- Client/SDK/Classes/BlockSource.cpp
+++ Client/SDK/Classes/BlockSource.cpp
@@ -7,23 +7,7 @@ auto BlockSource::getBlock(Vec3<int>* blockPos) -> Block* {
     static auto _GetBlock = (GetBlock)nullptr;
 
     if(_GetBlock == nullptr)
-        switch(Minecraft::sdkVer){
-            case MC_VER::v1_18_1_20:
-                _GetBlock = (GetBlock)(Mem::findSig("48 89 5C 24 ? 57 48 83 EC 20 48 8B F9 48 8B DA 8B 4A 04 0F BF 47 3A"));
-            break;
-            case MC_VER::v1_18_0_02:
-                _GetBlock = (GetBlock)(Mem::findSig("48 89 5C 24 ? 57 48 83 EC 20 48 8B F9 48 8B DA 8B 4A 04 0F BF 47 3A"));
-            break;
-            case MC_VER::v1_17_41_1:
-                _GetBlock = (GetBlock)(Mem::findSig("48 89 5C 24 ? 57 48 83 EC 20 48 8B F9 48 8B DA 8B 4A 04 0F BF 47 3A"));
-            break;
-            case MC_VER::v1_17_40_6:
-                _GetBlock = (GetBlock)(Mem::findSig("48 89 5C 24 ? 57 48 83 EC 20 48 8B F9 48 8B DA 8B 4A 04 0F BF 47 3A"));
-            break;
-            case MC_VER::v1_17_34_2:
-                _GetBlock = (GetBlock)(Mem::findSig("48 89 5C 24 ? 57 48 83 EC 20 48 8B F9 48 8B DA 8B 4A 04 0F BF 47"));
-            break;
-        };
+        _GetBlock = (GetBlock)(Mem::findSig("48 89 5C 24 ? 57 48 83 EC 20 48 8B F9 48 8B DA 8B 4A 04 0F BF 47 3A"));
 
     return _GetBlock(this, blockPos);
 };
