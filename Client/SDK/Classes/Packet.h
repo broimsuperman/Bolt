d817f15ea035b8af462aec0bffc9e622f79e1749
EchoHackCmd <EchoHackCmd@gmail.com>
2021-12-25 03:17:02 +0000

    Update Packet.h

--- Client/SDK/Classes/Packet.h
+++ Client/SDK/Classes/Packet.h
@@ -68,7 +68,25 @@ public:
 
 class PlayerAuthInputPacket : public Packet {
 public:
-    PlayerAuthInputPacket();
+    PlayerAuthInputPacket(Vec3<float> position = Vec3<float>(0.f, 0.f, 0.f), Vec2<float> bodyRot = Vec2<float>(0.f, 0.f), Vec3<float> velocity = Vec3<float>(0.f, 0.f, 0.f));
+private:
+	char pad_0008[40]; //0x0008
+public:
+	Vec2<float> rotation;   //0x0030
+	Vec3<float> position;   //0x0038
+	float headYaw;          //0x0044
+	Vec3<float> velocity;   //0x0048
+private:
+	char pad_0054[28];      //0x0054
+public:
+	uint8_t inputMode;      //0x0070
+private:
+	char pad_0071[7];       //0x0071
+public:
+	uint8_t clientPlayMode; //0x0078
+public:
+    auto getInputMode(void) -> std::pair<uint8_t, std::string>;
+    auto getClientPlayMode(void) -> std::pair<uint8_t, std::string>;
 };
 
 #endif /* CLIENT_SDK_CLASSES_PACKET */
