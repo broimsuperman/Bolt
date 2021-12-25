03f97d798205447568da45c162d6562f69cdb0a9
EchoHackCmd <EchoHackCmd@gmail.com>
2021-12-25 06:40:06 +0000

    Update PlayerInventory.cpp

--- Client/SDK/Classes/PlayerInventory.cpp
+++ Client/SDK/Classes/PlayerInventory.cpp
@@ -8,6 +8,9 @@ auto Inventory::getItem(int slot) -> ItemStack* {
 
     if(_GetItem == nullptr)
         switch(Minecraft::sdkVer) {
+            case MC_VER::v1_18_2_30:
+                _GetItem = (GetItem)(this->VTable[5]);
+            break;
             case MC_VER::v1_18_1_20:
                 _GetItem = (GetItem)(this->VTable[5]);
             break;
@@ -32,6 +35,9 @@ auto PlayerInventory::getInventory(void) -> Inventory* { // PlayerInventory::get
     auto inventory = (Inventory*)nullptr;
 
     switch(Minecraft::sdkVer) {
+        case MC_VER::v1_18_2_30:
+            inventory = *(Inventory**)((uintptr_t)(this) + 0xB0);
+        break;
         case MC_VER::v1_18_1_20:
             inventory = *(Inventory**)((uintptr_t)(this) + 0xB0);
         break;
