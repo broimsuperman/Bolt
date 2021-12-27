8f7e99296057d452cfea84e3a7b1f4c243e6aabb
EchoHackCmd <EchoHackCmd@gmail.com>
2021-12-27 00:25:32 +0000

    Update BunnyHop.cpp

--- Client/Bolt/Module/Modules/Movement/BunnyHop.cpp
+++ Client/Bolt/Module/Modules/Movement/BunnyHop.cpp
@@ -8,6 +8,16 @@ auto BunnyHop::onGameMode(GameMode* GM) -> void {
     if(GM == nullptr || GM->player == nullptr)
         return;
     
+    auto player = GM->player;
+    
+    if(this->canUse() && *player->onGround() && !player->isJumping())
+        player->jumpFromGround();
+};
+
+auto BunnyHop::canUse(void) -> bool {
+    auto instance = Minecraft::getClientInstance();
+    auto mcGame = (instance != nullptr ? instance->getMinecraftGame() : nullptr);
+
     auto manager = this->getManager();
     
     auto wKey = manager->isHoldingKey(0x57);
@@ -15,11 +25,5 @@ auto BunnyHop::onGameMode(GameMode* GM) -> void {
     auto aKey = manager->isHoldingKey(0x41);
     auto dKey = manager->isHoldingKey(0x44);
 
-    if(!(wKey || sKey || aKey || dKey))
-        return;
-    
-    auto player = GM->player;
-    
-    if(*player->onGround() && !player->isJumping())
-        player->jumpFromGround();
+    return (mcGame->canUseKeys() ? (wKey || sKey || aKey || dKey) : false);
 };
