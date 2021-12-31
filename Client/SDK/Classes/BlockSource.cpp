7b8e8f2bac1eac9639e11f7e1536359a3c0dfbc8
EchoHackCmd <EchoHackCmd@gmail.com>
2021-12-31 14:43:22 +0000

    Update BlockSource.cpp

--- Client/SDK/Classes/BlockSource.cpp
+++ Client/SDK/Classes/BlockSource.cpp
@@ -2,17 +2,16 @@
 
 #include "../Minecraft.h"
 
-auto BlockSource::getBlock(Vec3<int>* blockPos) -> Block* {
+auto BlockSource::getBlock(Vec3<int> blockPos) -> Block* {
     using GetBlock = Block* (__thiscall*)(BlockSource*, Vec3<int>*);
     static auto _GetBlock = (GetBlock)nullptr;
 
     if(_GetBlock == nullptr)
         _GetBlock = (GetBlock)(Mem::findSig("48 89 5C 24 ? 57 48 83 EC 20 48 8B F9 48 8B DA 8B 4A 04 0F BF 47 3A"));
 
-    return _GetBlock(this, blockPos);
+    return _GetBlock(this, &blockPos);
 };
 
-auto BlockSource::getBlock(Vec3<float>* blockPos) -> Block* {
-    auto blockPosInt = Vec3<int>((int)blockPos->x, (int)blockPos->y, (int)blockPos->z);
-    return this->getBlock(&blockPosInt);
+auto BlockSource::getBlock(Vec3<float> blockPos) -> Block* {
+    return this->getBlock(Vec3<int>((int)blockPos.x, (int)blockPos.y, (int)blockPos.z));
 };
