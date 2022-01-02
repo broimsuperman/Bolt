12dc7c1b5303002e76754192621543687d290617
EchoHackCmd <EchoHackCmd@gmail.com>
2022-01-02 12:32:36 +0000

    Update TeleportCommand.cpp

--- Client/Bolt/Command/Commands/TeleportCommand.cpp
+++ Client/Bolt/Command/Commands/TeleportCommand.cpp
@@ -4,8 +4,6 @@
 #include "../../../Client.h"
 
 auto TeleportCommand::onCommand(std::string message, std::vector<std::string> args) -> void {
-    if(args.size() < 3)
-        this->reply(std::string("Invalid usage, example: " + this->getManager()->cmdPrefix + this->name + " 75 255 120"));
     try {
         auto instance = Minecraft::getClientInstance();
         auto player = (instance != nullptr ? instance->getLocalPlayer() : nullptr);
@@ -14,8 +12,9 @@ auto TeleportCommand::onCommand(std::string message, std::vector<std::string> ar
             return;
         
         player->setPos(Vec3<float>(std::stof(args.at(1)), std::stof(args.at(2)), std::stof(args.at(3))));
+        this->reply(std::string("Teleported, X: " + args.at(1) + ", Y: " + args.at(2) + ", Z: " + args.at(3)));
 
     } catch(std::exception e) {
-        this->reply("Invalid arguments");
+        this->reply(std::string("Invalid usage, example: " + this->getManager()->cmdPrefix + this->name + " 75 255 120"));
     };
 };
