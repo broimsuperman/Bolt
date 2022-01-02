90a4eb26839aaea46fd895c56a10e9f05f6461cb
EchoHackCmd <EchoHackCmd@gmail.com>
2022-01-02 22:42:24 +0000

    ~ argument for teleport command

--- Client/Bolt/Command/Commands/TeleportCommand.cpp
+++ Client/Bolt/Command/Commands/TeleportCommand.cpp
@@ -7,9 +7,18 @@ auto TeleportCommand::onCommand(std::string message, std::vector<std::string> ar
     try {
         auto instance = Minecraft::getClientInstance();
         auto player = (instance != nullptr ? instance->getLocalPlayer() : nullptr);
+        auto pos = (player != nullptr ? *player->getPos() : Vec3<float>(0, 0, 0));
 
         if(player == nullptr)
             return;
+        
+        for(auto I = 1; I < args.size(); I++) {
+            auto arg = args.at(I);
+            if(arg.rfind("~") == 0) {
+                args.at(I).erase(0, 1);
+                args.at(I) = std::string(std::to_string((I == 1 ? pos.x : I == 2 ? pos.y : pos.z) + (arg.length() > 1 ? std::stof(args.at(I)) : 0)));
+            };
+        };
         
         player->setPos(Vec3<float>(std::stof(args.at(1)), std::stof(args.at(2)), std::stof(args.at(3))));
         this->reply(std::string("Teleported, X: " + args.at(1) + ", Y: " + args.at(2) + ", Z: " + args.at(3)));
