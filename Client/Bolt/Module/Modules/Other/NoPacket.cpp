1992bb347014c1c1821865b1f6b57210e6a4a3e3
EchoHackCmd <EchoHackCmd@gmail.com>
2022-01-06 00:29:08 +0000

    Update NoPacket.cpp

--- Client/Bolt/Module/Modules/Other/NoPacket.cpp
+++ Client/Bolt/Module/Modules/Other/NoPacket.cpp
@@ -1,5 +1,10 @@
 #include "NoPacket.h"
 
 auto NoPacket::onPacket(Packet* packet, bool* cancel) -> void {
+    auto name = packet->getName();
+
+    if(name.rfind("InteractPacket") == 0 || name.rfind("ContainerClosePacket") == 0/* || name.rfind("ItemStackRequest") == 0*/)
+        return;
+    
     *cancel = true;
 };
